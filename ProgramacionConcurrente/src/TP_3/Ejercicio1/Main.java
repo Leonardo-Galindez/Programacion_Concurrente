4155212d9db7a1004d827244be71541ff8cfd90a
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2023-08-28 00:27:51 -0300

    ejercicio 1 TP 3 terminado

---
+++ ProgramacionConcurrente/src/TP_3/Ejercicio1/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TP_3.Ejercicio1;
+
+/**
+ *
+ * @author galin
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        VerificarCuenta vc = new VerificarCuenta();
+        Thread Luis = new Thread(vc, "Luis");
+        Thread Manuel = new Thread(vc, "Manuel");
+        Luis.start();
+        Manuel.start();
+    }
+}
