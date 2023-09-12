f7055ebd57148c68e8be56c2a1f6be421e7d00c8
LeonardoGalindez <88455461+LeonardoGalindez@users.noreply.github.com>
2023-09-12 15:11:43 -0300

    cambios

---
+++ ProgramacionConcurrente/src/TP_3/Ejercicio5/Auto.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TP_3.Ejercicio5;
+
+/**
+ *
+ * @author galin
+ */
+public class Auto {
+
+    private String matricula;
+    private String modelo;
+    private int km;
+    private String marca;
+    private int litrosCombustible;
+
+    public Auto(String matricula, String modelo, int km, String marca) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.km = km;
+        this.marca = marca;
+    }
+
+    public int getLitrosCombustible() {
+        return litrosCombustible;
+    }
+
+    public void setLitrosCombustible(int litrosCombustible) {
+        this.litrosCombustible = litrosCombustible;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getKm() {
+        return km;
+    }
+
+    public void setKm(int km) {
+        this.km = km;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void recorrerKilometros(int km) {
+        this.km = this.km + km;
+
+    }
+
+    //cada 10 km comsume 1 litro de combustible
+    public void consumoCombustible(int km) {
+        if (this.litrosCombustible > (km / 10)) {
+            this.litrosCombustible = this.litrosCombustible - km / 10;
+        } else {
+            //reserva
+            System.out.println("Reserva");
+            System.out.println("");
+            System.out.println("Debe cargar Combustible");
+        }
+
+    }
+
+}
---
+++ ProgramacionConcurrente/src/TP_3/Ejercicio5/Vehiculo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TP_3.Ejercicio5;
+
+/**
+ *
+ * @author galin
+ */
+public class Vehiculo {
+
+   
+}
