f8035ee9674e5eb2b1adf0390cbc4c48e756b8bd
@HerWar <88455461+Leonardo-Galindez@users.noreply.github.com>
2023-11-20 04:38:11 -0300

    recurso compartido planteado

--- ProgramacionConcurrente/src/Parcial2023/Planta.java
+++ ProgramacionConcurrente/src/Parcial2023/Planta.java
@@ -6,16 +6,21 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Planta {
     private Lock acceso;
-    private Lock embotellador;
+    // private Lock embotellador;
 
     private Condition embotelladorVino;
     private Condition embotelladorAgua;
 
     private Condition empaquetador;
+
     private Condition transportador;
 
     private int contVino = 0;
     private int contAgua = 0;
+    private int cajas = 0;
+    private boolean empacar = false;
+    private boolean transportar = false;
+    private boolean empaquetando = false;
 
     public Planta() {
         this.acceso = new ReentrantLock();
@@ -27,6 +32,7 @@ public class Planta {
         this.embotelladorAgua = acceso.newCondition();
 
         this.empaquetador = acceso.newCondition();
+
         this.transportador = acceso.newCondition();
     }
 
@@ -35,21 +41,22 @@ public class Planta {
     public void prepararBotellaVino() {
         try {
             acceso.lock();
-            while (contVino >= 10) {
+            while (contVino >= 10 || empaquetando) {
                 embotelladorVino.await();
             }
             contVino++;
             System.out.println("El embotellador " + Thread.currentThread().getName() + " preparo un vino");
             if (contVino == 10) {
                 System.out.println("CAJA DE VINO LLENA!!!");
+                empacar = true;
                 empaquetador.signalAll();
+                // mandar señal que es caja de agua ejemplo: mandar una "V" o algo asi
             }
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         } finally {
             acceso.unlock();
         }
-
     }
 
     public void prepararBotellaAgua() {
@@ -61,8 +68,10 @@ public class Planta {
             contAgua++;
             System.out.println("El embotellador " + Thread.currentThread().getName() + " preparo un agua");
             if (contVino == 10) {
-                System.out.println("CAJA DE AGUA LLENA!!!");
+                System.out.println("CAJA DE AGUA LLENA !!!");
+                empacar = true;
                 empaquetador.signalAll();
+                // mandar señal que es caja de agua ejemplo: mandar una "A" o algo asi
             }
         } catch (InterruptedException ex) {
             ex.printStackTrace();
@@ -73,25 +82,67 @@ public class Planta {
 
     // Empaquetador
 
-    public void empaquetarVino() {
-
+    public void empaquetarCaja() {
+        try {
+            acceso.lock();
+            while (!empacar) {
+                empaquetador.await();
+            }
+            System.out.println("El empaquetador " + Thread.currentThread().getName() + " guarda la caja de vino");
+            cajas++;
+            empaquetando = true;
+            if (cajas == 10) {
+                System.out.println("Almacen Lleno !!!");
+                transportar = true;
+                transportador.signalAll();
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } finally {
+            acceso.unlock();
+        }
     }
 
-    public void empaquetarAgua() {
-
+    public void reponerCajaVino() {
+        try {
+            acceso.lock();
+            contVino = 0;
+            System.out.println("El empaquetador " + Thread.currentThread().getName() + " repone caja de vinos");
+            empaquetando = false;
+            embotelladorVino.signalAll();
+            embotelladorAgua.signalAll();
+        } finally {
+            acceso.unlock();
+        }
     }
 
-    public void guardarCaja() {
-
-    }
-
-    public void reponerCaja() {
-
+    public void reponerCajaAgua() {
+        try {
+            acceso.lock();
+            contAgua = 0;
+            System.out.println("El empaquetador " + Thread.currentThread().getName() + " repone caja de agua");
+            empaquetando = false;
+            embotelladorAgua.signalAll();
+            embotelladorVino.signalAll();
+        } finally {
+            acceso.unlock();
+        }
     }
 
     // Transportador
 
     public void repartirCajas() {
-
+        try {
+            acceso.lock();
+            while (!transportar) {
+                transportador.await();
+            }
+            System.out.println("El transportador " + Thread.currentThread().getName() + " esta repartiendo las cajas");
+            cajas = 0;
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } finally {
+            acceso.unlock();
+        }
     }
 }
